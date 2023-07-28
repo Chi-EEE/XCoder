157e474cbdf3298d5a117ade189a0b65247b9427
Chi Huu Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-07-29 00:40:39 +0100

    Basic code for running SupercellFlash

--- XCoder/src/main.cpp
+++ XCoder/src/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include "SupercellFlash.h"
 
 using namespace std;
+using namespace sc;
 
-int main(int argc, char** argv)
-{
-    cout << "hello world!" << endl;
+int main(int argc, char* argv[]) {
+    // The first argument (argv[0]) is the program name
+    // The following arguments (argv[1] onwards) are the file paths
+
+    // Check if any file paths were provided
+    if (argc > 1) {
+        // Iterate over the file paths
+        for (int i = 1; i < argc; ++i) {
+            SupercellSWF swf;
+            try {
+                swf.load(argv[i]);
+            }
+            catch (const std::exception& err) {
+                cout << "Failed to load file. Error: " << err.what() << endl;
+            }
+            cout << "File has: " << endl;
+            cout << swf.exports.size() << " export names" << endl;
+            cout << swf.textures.size() << " textures" << endl;
+            cout << swf.textFields.size() << " textfields" << endl;
+            cout << swf.shapes.size() << " shapes" << endl;
+            cout << swf.movieClips.size() << " movieclips" << endl;
+
+        }
+    }
+    else {
+        std::cout << "No file paths provided." << std::endl;
+    }
+
     return 0;
 }
